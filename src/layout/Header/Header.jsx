1c5bb54f988b51afb96ac909da767e9fea8c26e3
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-11 22:35:36 +0700

    top marquee

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import { FiMusic, FiSettings } from 'react-icons/fi';
 import { BsMusicNoteList } from 'react-icons/bs';
+import ReactMarquee from 'react-fast-marquee';
+import { useLocation } from 'react-router-dom';
 import {
-  HeaderWrapper, Brand, Icons, IconItem,
+  HeaderWrapper, Brand, Icons, IconItem, CoolMarquee, MarqueeContent,
 } from './Header.style';
 import { NavLink } from '../../common/Link';
+import useActiveSongStore from '../../store/useActiveSongStore';
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const activeSong = useActiveSongStore((state) => state.activeSong.path);
+  const isPlay = useActiveSongStore((state) => state.activeSong.isPlay);
+  const title = activeSong.split('\\').pop().replace(/\.[^/.]+$/, '');
+
+  const showMarquee = pathname !== '/';
+
   return (
     <HeaderWrapper>
       <Brand>
         Musica
       </Brand>
+      {
+         activeSong && (
+         <CoolMarquee>
+           <ReactMarquee
+             style={{
+               height: '100%',
+               display: 'flex',
+               alignItems: 'center',
+               opacity: showMarquee ? 1 : 0,
+             }}
+             gradient={false}
+             speed={10}
+             play={isPlay}
+           >
+             <MarqueeContent>
+               {title}
+             </MarqueeContent>
+           </ReactMarquee>
+         </CoolMarquee>
+         )
+      }
       <Icons>
         <NavLink
           exact
--- src/layout/Header/Header.style.jsx
+++ src/layout/Header/Header.style.jsx
@@ -15,6 +15,7 @@ export const Brand = styled.div`
     font-semibold
     text-3xl
     text-accent
+    w-1/3
   `}
 `;
 
@@ -24,6 +25,8 @@ export const Icons = styled.div`
     items-center
     h-full
     gap-x-10
+    w-1/3
+    justify-end
   `}
 
   .link-active{
@@ -37,5 +40,20 @@ export const IconItem = styled.div`
   ${tw`
     text-2xl
     h-full
+  `}
+`;
+
+export const CoolMarquee = styled.div`
+  ${tw`
+    w-1/2
+    text-accent 
+    text-lg 
+    h-full 
+  `}
+`;
+
+export const MarqueeContent = styled.div`
+  ${tw`
+    mr-8
   `}
 `;
